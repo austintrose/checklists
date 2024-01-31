3093fcb72757c084404828c73462b2755e8a3914
Austin Rose <austin.rose94@gmail.com>
2024-01-31 16:13:54 -0500

    wip

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import yaml
 from flask import Flask
 from flask import render_template
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
 
 @app.route("/warrior_iii_bedford")
 def warrior_iii_bedford():
-    with open('./data.yml', 'r') as f:
+    with open('data/eac_bedford/warrior_iii.yml', 'r') as f:
         data = yaml.safe_load(f)
     return render_template('warrior_iii_bedford/landscape.html.j2', data=data)
-
-
